4eac34a478a0e9240325e1cc0eb3009bf8365c59
Hsin-Mao Wu <hsinmao@maoshouse.com>
2017-04-24 22:39:48 -0700

    init
    just a bunch of stubs

---
+++ src/finding_neno/JobWorkflowInitiator.java
@@ -0,0 +1,14 @@
+package finding_neno;
+
+import Prism.core.AbstractImplementation;
+import Prism.core.Event;
+
+public class JobWorkflowInitiator extends AbstractImplementation {
+
+    @Override
+    public void handle(Event event) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/finding_neno/ResultBatchNotifier.java
@@ -0,0 +1,14 @@
+package finding_neno;
+
+import Prism.core.AbstractImplementation;
+import Prism.core.Event;
+
+public class ResultBatchNotifier extends AbstractImplementation {
+
+    @Override
+    public void handle(Event event) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
