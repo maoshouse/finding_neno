3298bc695868bbb78d66e2132261747d592d3c70
Hsin-Mao Wu <hsinmao@maoshouse.com>
2017-04-28 15:40:59 -0700

    implement http post

--- src/findingneno/components/ResultBatchNotifier.java
+++ src/findingneno/components/ResultBatchNotifier.java
@@ -1,14 +1,44 @@
 package findingneno.components;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 import Prism.core.AbstractImplementation;
 import Prism.core.Event;
+import findingneno.Job.Job;
+import findingneno.components.ComponentConstants.EventConstants;
 
 public class ResultBatchNotifier extends AbstractImplementation {
 
     @Override
     public void handle(Event event) {
-	// TODO Auto-generated method stub
+	Job job = (Job) event.getParameter(EventConstants.JOB_PARAMETER);
+	String value = (String) event.getParameter(EventConstants.NEW_VALUE_PARAMETER);
+	int statusCode = -1;
+	try {
+	    statusCode = sendPostRequest(job, value);
+	} catch (Exception e) {
+	    // handle
+	}
 
+	if (statusCode < 200 || statusCode >= 300) {
+	    // notify initiator that notification failed, log it and try
+	    // initiating again.
+	}
     }
 
+    public int sendPostRequest(Job job, String value) throws ClientProtocolException, IOException {
+	HttpClient httpClient = HttpClientBuilder.create().build();
+	HttpPost httpPost = new HttpPost("");
+	httpPost.addHeader("User-Agent", "lol");
+
+	HttpResponse httpResponse = httpClient.execute(httpPost);
+	int statusCode = httpResponse.getStatusLine().getStatusCode();
+	return statusCode;
+    }
 }
