84a773c5fdcbe8b3677ae41dd72cbe96b124b825
Hsin-Mao Wu <hsinmao@maoshouse.com>
2017-04-29 11:44:59 -0700

    implmeent blacklisted jobs

--- src/findingneno/components/JobScheduler.java
+++ src/findingneno/components/JobScheduler.java
@@ -1,5 +1,9 @@
 package findingneno.components;
 
+import java.util.HashSet;
+
+import org.apache.commons.lang3.StringUtils;
+
 import Prism.core.AbstractImplementation;
 import Prism.core.Event;
 import Prism.core.PrismConstants;
@@ -7,30 +11,31 @@ import findingneno.Job.Job;
 import findingneno.components.ComponentConstants.EventConstants;
 
 public class JobScheduler extends AbstractImplementation {
+    private HashSet<String> blacklistedJobs;
 
-    @Override
-    public void start() {
-
+    public JobScheduler() {
+	blacklistedJobs = new HashSet<String>();
     }
 
     @Override
     public void handle(Event event) {
+	Job job = (Job) event.getParameter(EventConstants.JOB_PARAMETER);
 	if (event.eventType == PrismConstants.REPLY) {
-	    // this was a notification from a higher layer
-	    // determine if this is from the dead queue, which should alert job
-	    // has failed several times and to stop queueing
-
-	    // if this is from open queue or another, then this means that this
-	    // job is good.
-
+	    if (StringUtils.equals(event.name, EventConstants.NOTIFICATION_BLACKLIST_JOB)) {
+		blacklistedJobs.add(job.toString());
+	    } else {
+		sendRequest(job);
+	    }
 	} else if (event.eventType == PrismConstants.REQUEST) {
-	    // this was a request from a lower layer
-	    Job job = (Job) event.getParameter(EventConstants.JOB_PARAMETER);
-	    Event requestEvent = new Event(EventConstants.EVENT_REQUEST_SCHEDULE_NEW_JOB);
-	    event.addParameter(EventConstants.JOB_PARAMETER, job);
-	    send(requestEvent);
+	    sendRequest(job);
 	}
-
     }
 
+    private void sendRequest(Job job) {
+	if (!blacklistedJobs.contains(job.toString())) {
+	    Event requestEvent = EventUtil.makeRequest(EventConstants.EVENT_REQUEST_SCHEDULE_NEW_JOB);
+	    requestEvent.addParameter(EventConstants.JOB_PARAMETER, job);
+	    send(requestEvent);
+	}
+    }
 }
